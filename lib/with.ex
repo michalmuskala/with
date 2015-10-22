4d13f90a6526c469fd8f5f379d40480c72264e68
Michal Muskala <michal@muskala.eu>
2015-10-23 00:58:07 +0200

    First naive implementation

--- lib/with.ex
+++ lib/with.ex
@@ -1,2 +1,29 @@
 defmodule With do
+
+  # defmacro with(transformer, do: pipeline) do
+  defmacro with(do: {:__block__, _, pipeline}) do
+    transform_pipe(pipeline)
+  end
+
+  defp transform_pipe([final]) do
+    final
+  end
+
+  defp transform_pipe([{:<-, _, [pattern, expr]} | rest]) do
+    quote do
+      case unquote(expr) do
+        unquote(pattern) ->
+          unquote(transform_pipe(rest))
+        unmached ->
+          unmached
+      end
+    end
+  end
+
+  defp transform_pipe([{:=, _, _} = assign | rest]) do
+    quote do
+      unquote(assign)
+      unquote(transform_pipe(rest))
+    end
+  end
 end
--- test/with_test.exs
+++ test/with_test.exs
@@ -1,8 +1,29 @@
 defmodule WithTest do
-  use ExUnit.Case
-  doctest With
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import With
+
+  test "matching" do
+    result =
+      with do
+        {:ok, x} <- {:ok, 1}
+        y = 2
+        {:ok, z} <- {:ok, 2 + 5}
+        {:ok, x + y + z}
+      end
+
+    assert result == {:ok, 10}
+  end
+
+  test "not matching" do
+    result =
+      with do
+        {:ok, x} <- {:ok, 1}
+        y = 2
+        {:ok, z} <- {:error, 123}
+        {:ok, x + y + z}
+      end
+
+    assert result == {:error, 123}
   end
 end
